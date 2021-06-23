96fa206f4fcdda64bd61023140ea59edd421e72f
EkawaD <55327310+EkawaD@users.noreply.github.com>
2021-06-23 17:32:25 +0200

    css

--- src/Form/PetType.php
+++ src/Form/PetType.php
@@ -5,7 +5,9 @@ namespace App\Form;
 use App\Entity\Pet;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -14,12 +16,12 @@ class PetType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('name')
-            ->add('species')
-            ->add('breed')
-            ->add('age')
-            ->add('weight')
-            ->add('sex')
+            ->add('name', TextType::class,  ["label" => "Nom"])
+            ->add('species',TextType::class,  ["label" => "Espèce"])
+            ->add('breed', TextType::class,  ["label" => "Race"])
+            ->add('age', IntegerType::class,  ["label" => "Age"])
+            ->add('weight',IntegerType::class,  ["label" => "Poids"])
+            ->add('sex', TextType::class,  ["label" => "Sexe"])
             ->add('image', FileType::class, ["label" => "Image (jpeg ou png)", 'data_class' => null])
             ->add('Add', SubmitType::class, ["label" => "Mettre l'animal en adoption"])
         ;
